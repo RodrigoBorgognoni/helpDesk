1be8d0e64248837b33bcff0e221c760b038420e1
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2025-03-03 13:48:44 -0300

    add normalize

--- header.php
+++ header.php
@@ -3,5 +3,6 @@
     <title>App Help Desk</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="/assets/css/estilos.css">
 </head>
