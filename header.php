0287e8a66d67f46f7e9f132596c936aa418ad5fe
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2025-03-03 15:38:40 -0300

    envio_post

--- header.php
+++ header.php
@@ -1,8 +1,11 @@
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Help Desk</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="/assets/css/estilos.css">
+
+    <link rel="icon" href="/assets/img/logo.png" type="image/x-icon">
 </head>
--- index.php
+++ index.php
@@ -10,19 +10,18 @@ include_once("header.php");
 
     <div class="container">
       <div class="row">
-
         <div class="card-login">
           <div class="card">
             <div class="card-header">
               Login
             </div>
             <div class="card-body">
-              <form>
+              <form action="/components/valida_login.php" method="post">
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
+                  <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
