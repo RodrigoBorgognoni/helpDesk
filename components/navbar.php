cdedc4fc9e869cce866ab93363cfbc111b901697
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2025-03-05 21:28:47 -0300

    add voltar home navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="/views/home.php">
         <img src="/assets/styles/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
     </a>
