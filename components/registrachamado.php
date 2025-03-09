dd3c3007d5c4993f58069cee0895104acfb460e1
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2025-03-09 12:49:46 -0300

    adicionamos verificaçao de diretório, se não existir ele será criado

--- components/registrachamado.php
+++ components/registrachamado.php
@@ -7,8 +7,16 @@ date_default_timezone_set('America/Sao_Paulo');
 // Gera o timestamp
 $timestamp = date('Y-m-d_H-i-s');
 
+// Diretório onde os arquivos de chamados serão armazenados
+$dir = $_SERVER['DOCUMENT_ROOT'] . '/arquivos/chamados/';
+
+// Verifica se o diretório existe, se não, cria o diretório
+if (!is_dir($dir)) {
+    mkdir($dir, 0777, true); // 0777 são as permissões e true permite a criação de diretórios aninhados
+}
+
 //função para abrir um arquivo, caso não exista, será criado
-$arquivo = fopen($_SERVER['DOCUMENT_ROOT'] . '/arquivos/chamados/chamado_' . $timestamp . '.txt', 'a'); //https://www.php.net/manual/en/function.fopen.php
+$arquivo = fopen($dir . 'chamado_' . $timestamp . '.txt', 'a'); //https://www.php.net/manual/en/function.fopen.php
 
 
 //função implode para juntar os valores do array em uma string delimitada por | e PHP_EOL para pular linha
